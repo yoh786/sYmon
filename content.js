f79f70ff4cbdd25bd68be2a8eb306b33080f13ab
yoh786 <yoh786@gmail.com>
2018-07-07 21:46:04 +0000

    done sec1

--- content.js
+++ content.js
@@ -1,7 +1,21 @@
 
 // Content.js
 // This script is functional code that can be executed in a webpage
+console.log("init symon writer");
+
+//example code
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if( request.message === "clicked_browser_action" ) {
+			//this is done on browser action click
+			var firstHref = $("a[href^='http']").eq(0).attr("href");
+			console.log(firstHref);
+
+		}
+	}
+);
 
 
-alert("Hello from the future");
-console.log("poo");
+//functional code
+console.log("el fin");
