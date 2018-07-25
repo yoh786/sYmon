25e5fb2dc9cd9e206d2e7f9026d1f1938e61147d
Y0H <yoh786@gmail.com>
2018-07-25 18:58:26 -0400

    created storage var, onInstall listener

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -2,6 +2,14 @@
 
 // Called when user clicks browser action.
 
+chrome.runtime.onInstalled.addListener(function() {
+// these run on installation or bootup of the extension
+
+	chrome.storage.sync.set({color: "#3aa757"}, function() {
+		console.log("made a storage var");
+	});
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// send message to active tab
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
