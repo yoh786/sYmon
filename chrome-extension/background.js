fa40d2775f392f30ee503d2cdfe61235731d2cdf
Y0H <yoh786@gmail.com>
2018-08-06 12:44:12 -0400

    milestone
    badge functions on click and it also sticks to current tab and resets on closing tab. tis beautiful.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -7,9 +7,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	   var activeTab = tabs[0];
 	   chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-
-		 chrome.browserAction.setBadgeText({text: 'COPY'});
+		 chrome.browserAction.setBadgeText({text: 'COPY', tabId: activeTab.id});
 	});
-
 
 });
